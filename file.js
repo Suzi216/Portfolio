51feb2524363e34a848a603b0e95d5e6ab15b032
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-07-18 13:01:22 +0200

    Fixing the background blur.

--- file.js
+++ file.js
@@ -1,12 +1,13 @@
 const ham = document.querySelector('.ham');
 const navbar = document.querySelector('.navigator-link');
 const x = document.querySelector('.x');
-
+const container = document.querySelector('.container');
 x.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
 ham.addEventListener('click', () => {
   navbar.classList.toggle('active');
+  container.classList.toggle('active');
 });
 document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', () => {
   navbar.classList.remove('active');
