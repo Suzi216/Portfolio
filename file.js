b0c7b8bcfb6c027affa1aea71fee04be7946bf2f
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-07-21 12:59:09 +0200

    stach

--- file.js
+++ file.js
@@ -3,7 +3,6 @@ const ham = document.querySelector('.ham');
 const navbar = document.querySelector('.navigator-link');
 const x = document.querySelector('.x');
 const container = document.querySelector('.container');
-// const popupsec = document.getElementById('.All-section');
 
 x.addEventListener('click', () => {
   navbar.classList.toggle('active');
@@ -17,11 +16,12 @@ document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', ()
   navbar.classList.remove('active');
   container.classList.remove('active');
 }));
+
 // popup window
 const myProjects = [
   {
     name: 'Tonic',
-    description: "Lorem Ipsum is simply dummy text of the printi my text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of typeng and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    description: "Lorem Ipsum is simply dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: 'images/SnapshootPortfolio.png',
     tech: ['Codekit', 'GitHub', 'Javascript', 'Bootstrap', 'Terminal', 'Codepen'],
     liveLink: 'https://suzi216.github.io/Portofolio/',
@@ -29,7 +29,7 @@ const myProjects = [
   },
   {
     name: 'Multi-Post Stories',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem my text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of typeIpsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    description:"Lorem Ipsum is simply dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: 'images/SnapshootPortfolio1.png',
     tech: ['Codekit', 'GitHub', 'Javascript', 'Bootstrap', 'Terminal', 'Codepen'],
     liveLink: 'https://suzi216.github.io/Portofolio/',
@@ -38,7 +38,7 @@ const myProjects = [
 
   {
     name: 'Facebook 360',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of typemy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    description: "Lorem Ipsum is simply dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     featuredImage: 'images/SnapshootPortfolio2.png',
     tech: ['Codekit', 'GitHub', 'Javascript', 'Bootstrap', 'Terminal', 'Codepen'],
     liveLink: 'https://suzi216.github.io/Portofolio/',
@@ -46,7 +46,7 @@ const myProjects = [
   },
   {
     name: 'Uber Navigation',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley my text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    description: "Lorem Ipsum is simply dummy  dummy text ever since the 1500s, when an unknown printer took a galley my text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. ",
     featuredImage: 'images/SnapshootPortfolio3.png',
     tech: ['Codekit', 'GitHub', 'Javascript', 'Bootstrap', 'Terminal', 'Codepen'],
     liveLink: 'https://suzi216.github.io/Portofolio/',
@@ -63,7 +63,7 @@ const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
 const parag = document.querySelector('.below-popup');
 const close = document.querySelector('.dis');
-const main = document.querySelector('.main');
+const body = document.querySelector('.body');
 const btns = [btn, btn1, btn2, btn3];
 function popups() {
   const title = document.createElement('h2');
@@ -84,13 +84,14 @@ function popups() {
       popup.style.display = 'block';
     };
     btns[i].addEventListener('click', () => {
-      main.classList.toggle('active');
+      body.classList.toggle('active');
+      popup.classList.toggle('active');
     });
   }
 
   close.onclick = function fuc() {
     popup.style.display = 'none';
-    main.classList.toggle('active');
+    body.classList.toggle('active');
   };
 }
 popups();
