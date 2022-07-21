6102fa09ac172bd72736ef70e6e2b4321445cb24
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-07-21 10:57:00 +0200

    Adding javascript to validate the form email

--- file.js
+++ file.js
@@ -1,3 +1,5 @@
+
+
 const ham = document.querySelector('.ham');
 const navbar = document.querySelector('.navigator-link');
 const x = document.querySelector('.x');
@@ -14,3 +16,29 @@ document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', ()
   navbar.classList.remove('active');
   container.classList.remove('active');
 }));
+
+// validation form
+
+//check if lowercase
+
+function isLowerCase(str)
+{
+    return str == str.toLowerCase() && str != str.toUpperCase();
+}
+
+
+// contact form button validation
+function myFunction() {
+  let email = document.getElementById("email").value;
+  if (isLowerCase(email)) {
+    function submitForm(event){
+         event.preventDefault();
+     }
+    text = "";
+  } else {
+    event.preventDefault();
+    text = "Input not Okay";
+    // console.log(email);
+  }
+  document.getElementById("error").innerHTML = text;
+}
